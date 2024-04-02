47e9b4007f62030c3b1ef48035e6a5bc134bac60
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2024-04-02 12:11:47 +0200

    clean mainfile comments

--- init.lua
+++ init.lua
@@ -4,17 +4,8 @@ require("prelude")
 require("plugin_manager")
 
 -- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup({
-  -- require("kickstart.plugins.autoformat"),
-  -- require("kickstart.plugins.debug"),
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/plugins/*.lua`
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'plugins' },
