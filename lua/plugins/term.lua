e4d17e167530d7e8668852cf25a5d1d3a5b4eb47
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2024-05-21 21:17:06 +0200

    add terminal plugin

---
+++ lua/plugins/term.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  opts = {
+    open_mapping = [[<c-\>]],
+    hide_numbers = true,
+    start_in_insert = true,
+    direction = "float",
+    shell = vim.o.shell,
+  }
+}
