ce16d5b4931023b818ac38dc7d1a73b00b84f850
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2024-08-02 16:35:47 +0200

    disable unused providers

--- lua/options.lua
+++ lua/options.lua
@@ -65,3 +65,8 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_go_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
