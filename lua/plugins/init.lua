50a03d963b8e3062174b8573bf2f799922eab70d
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2024-08-07 13:44:17 +0200

    use native comment keymap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,16 +24,8 @@ return {
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
     opts = {},
-  },
-
-  -- "gc" to comment visual regions/lines
-  {
-    'numToStr/Comment.nvim',
-    opts = {}
   },
 
   -- Highlight todo, notes, etc in comments
