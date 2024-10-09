55dce9930291264575ae2cb1402fd1ebb97c8027
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2024-10-09 10:19:44 +0200

    add xmake support

---
+++ lua/plugins/xmake.lua
@@ -0,0 +1,11 @@
+if vim.fn.executable('xmake') == 1 then
+  return {
+    "Mythos-404/xmake.nvim",
+    lazy = true,
+    event = "BufReadPost xmake.lua",
+    config = true,
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  }
+end
+
+return {}
