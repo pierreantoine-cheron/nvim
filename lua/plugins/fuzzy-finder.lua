82b77e4b9e0f21a689b9cb30e43ed65b8fafda18
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2024-10-09 10:16:42 +0200

    refacto telescope config

--- lua/plugins/fuzzy-finder.lua
+++ lua/plugins/fuzzy-finder.lua
@@ -1,3 +1,28 @@
+-- Telescope live_grep in git root
+-- Function to find the git root directory based on the current buffer's path
+local function find_git_root()
+  -- Use the current buffer's path as the starting point for the git search
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_dir
+  local cwd = vim.fn.getcwd()
+  -- If the buffer is not associated with a file, return nil
+  if current_file == '' then
+    current_dir = cwd
+  else
+    -- Extract the directory from the current file's path
+    current_dir = vim.fn.fnamemodify(current_file, ':h')
+  end
+
+  -- Find the Git root directory from the current file's path
+  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')
+      [1]
+  if vim.v.shell_error ~= 0 then
+    print 'Not a git repository. Searching on current working directory'
+    return cwd
+  end
+  return git_root
+end
+
 return {
   {
     'nvim-telescope/telescope-fzf-native.nvim',
@@ -17,22 +42,26 @@ return {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     config = function()
-      require('telescope').setup {
-        defaults = {
-          mappings = {
-            i = {
-              ['<C-u>'] = false,
-              ['<C-d>'] = false,
-              -- ['<c-enter>'] = 'to_fuzzy_refine',
-            },
-          },
-        },
-        -- extensions = {
-        --   ['ui-select'] = {
-        --     require('telescope.themes').get_dropdown(),
+      local telescope = require('telescope')
+
+      telescope.setup {
+        -- defaults = {
+        --   mappings = {
+        --     i = {
+        --       ['<C-u>'] = false,
+        --       ['<C-d>'] = false,
+        --       -- ['<c-enter>'] = 'to_fuzzy_refine',
+        --     },
         --   },
         -- },
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown(),
+          },
+        },
       }
+
+      telescope.load_extension("file_browser")
 
       -- Enable telescope extensions, if installed
       pcall(require('telescope').load_extension, 'fzf')
@@ -41,31 +70,6 @@ return {
       local builtins = require('telescope.builtin')
       -- See `:help telescope.builtin`
       SetTelescopeKeymap(builtins)
-
-      -- Telescope live_grep in git root
-      -- Function to find the git root directory based on the current buffer's path
-      local function find_git_root()
-        -- Use the current buffer's path as the starting point for the git search
-        local current_file = vim.api.nvim_buf_get_name(0)
-        local current_dir
-        local cwd = vim.fn.getcwd()
-        -- If the buffer is not associated with a file, return nil
-        if current_file == '' then
-          current_dir = cwd
-        else
-          -- Extract the directory from the current file's path
-          current_dir = vim.fn.fnamemodify(current_file, ':h')
-        end
-
-        -- Find the Git root directory from the current file's path
-        local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')
-            [1]
-        if vim.v.shell_error ~= 0 then
-          print 'Not a git repository. Searching on current working directory'
-          return cwd
-        end
-        return git_root
-      end
 
       -- Custom live_grep function to search in git root
       local function live_grep_git_root()
