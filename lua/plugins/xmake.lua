66626050ff05e7acfd729ce3cd124bf9bd1e85b0
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2025-07-09 18:09:26 +0200

    add compile command gen when using xmake

--- lua/plugins/xmake.lua
+++ lua/plugins/xmake.lua
@@ -3,7 +3,16 @@ if vim.fn.executable('xmake') == 1 then
     "Mythos-404/xmake.nvim",
     lazy = true,
     event = "BufReadPost xmake.lua",
-    config = true,
+    opts = {
+      on_save = {
+        -- Configuration for generating `compile_commands.json`
+        lsp_compile_commands = {
+          enable = true,
+          -- Directory name (relative path) for output file
+          output_dir = "./",
+        },
+      },
+    },
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
   }
 end
